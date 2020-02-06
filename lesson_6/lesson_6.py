001b4a26895d2720467797855ea64210a37257b9
Koyang-Kukeshov Michel <michelkoyangkuleshov@gmail.com>
2020-02-06 23:52:43 +0300

    Выполнил задание №6

--- lesson_6/lesson_6.py
+++ lesson_6/lesson_6.py
@@ -1,13 +1,38 @@
 # Подсчитать, сколько было выделено памяти под переменные в ранее разработанных
 # программах в рамках первых трех уроков. Проанализировать результат и определить
 # программы с наиболее эффективным использованием памяти
-from timeit import timeit
+from sys import getsizeof
+from random import choice
 
 # 2.3. Сформировать из введенного числа обратное по порядку входящих в него цифр и вывести на экран.
 # Например, если введено число 3486, то надо вывести число 6843.
 
+
+def show_size(*objects):
+    spam_size = getsizeof(objects)
+    for obj in objects:
+        if hasattr(obj, '__iter__'):
+            if hasattr(obj, 'items'):
+                for key, value in obj.items():
+                    show_size(key)
+                    show_size(value)
+                    spam_size += getsizeof(obj)
+            elif not isinstance(obj, str):
+                for item in obj:
+                    show_size(item)
+                    spam_size += getsizeof(obj)
+            else:
+                spam_size += getsizeof(obj)
+        else:
+            spam_size += getsizeof(obj)
+    return spam_size
+
+
 # Алгоритм №1
 def get_str1(temp_str):
+    obj_size = 0
+    if isinstance(temp_str, int):
+        temp_str = str(temp_str)
     str2 = ''
     len_str1 = len(temp_str)
     if len(str2) != len_str1 and len(temp_str) > 1:
@@ -15,54 +40,30 @@ def get_str1(temp_str):
         get_str1(temp_str[:len(temp_str) - 1])
     else:
         str2 += temp_str
-    # print(f'Число {temp_str} в обратном порядке - {str2}')
-# str1 = input('Введите число: ')
-print(timeit('get_str1("1234")', number=100, globals=globals()))
-# 0.00012478299686335959
-print(timeit('get_str1("12345678")', number=100, globals=globals()))
-# 0.00028283000210649334
-print(timeit('get_str1("1234567812345678")', number=100, globals=globals()))
-# 0.0005897980008739978
-print(timeit('get_str1("12345678123456781234567812345678")', number=100, globals=globals()))
-# 0.0012418559999787249
-print(timeit('get_str1("1234567812345678123456781234567812345678123456781234567812345678")', number=100, globals=globals()))
-# 0.0023380669990729075
-
-print('-'*100)
+    obj_size += show_size(str2, len_str1, temp_str)
+    return obj_size
 
 # Алгоритм №2
-# num = int(str1)
 def get_str2(num_2):
+    if isinstance(num_2, str):
+        num_2 = int(num_2)
     result = 0
     while num_2 > 0:
         result = result * 10 + num_2 % 10
         num_2 = num_2 // 10
-print(timeit('get_str2(1234)', number=100, globals=globals()))
-# 4.179600000497885e-05
-print(timeit('get_str2(12345678)', number=100, globals=globals()))
-# 9.166699965135194e-05
-print(timeit('get_str2(1234567812345678)', number=100, globals=globals()))
-# 0.00022185600028024055
-print(timeit('get_str2(12345678123456781234567812345678)', number=100, globals=globals()))
-# 0.00047755099876667373
-print(timeit('get_str2(1234567812345678123456781234567812345678123456781234567812345678)', number=100, globals=globals()))
-# 0.001069234000169672
+    return show_size(result, num_2)
 
-print('-'*100)
 
-# Алгоритм №3
-print(timeit('resule2 = "1234"[::-1]', number=100, globals=globals()))
-# 7.406000804621726e-06
-print(timeit('resule2 = "12345678"[::-1]', number=100, globals=globals()))
-# 7.680002454435453e-06
-print(timeit('resule2 = "1234567812345678"[::-1]', number=100, globals=globals()))
-# 1.6488997061969712e-05
-print(timeit('resule2 = "123456781234567812345678"[::-1]', number=100, globals=globals()))
-# 8.413000614382327e-06
-print(timeit('resule2 = "123456781234567812345678123456781234567812345678"[::-1]', number=100, globals=globals()))
-# 1.905599856399931e-05
+n = 10
+str_to_reverse = ''
+for i in range(n):
+    str_to_reverse += str(choice(list({1, 2, 3, 4, 5, 6, 7, 8, 9, 0})))
 
-# Вывод: Несмотря на то, что я доработал свою первый алгоритм - избавился от глобальных
-# переменных, он всё равно оказался медленнее, чем вариант 2 и вариант 3.
-# Я думаю можно немного улучшить 1ый вариант, если вычислять длинну строки вне
-# функции и потом передавать в функцию. Так как len вызывается 78 раз.
+print(f'Реализация №1: {get_str1(str_to_reverse)} байт') # 209 байт
+print(f'Реализация №2: {get_str2(str_to_reverse)} байт') # 120 байт
+print(f'Реализация №3: {show_size(str_to_reverse[::-1])} байт') # 115 байт
+
+# Вывод:
+# Наиболее эффективным оказалась встроенная функция, хотя и алгоритм №2 позазал
+# себя сравнительно не плохо. Функция №1 из-за многократного вызова и передачи
+# аргументов требует больше всего памяти.
